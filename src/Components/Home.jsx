6b179ec8b13e944c3ebfbaeae2ec2f0f7f19822c
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-11-08 22:45:52 +0600

    add a banner

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Banner from "./Banner";
 
 const Home = () => {
   return (
     <div>
-      <h2>This is Home</h2>
+      <Banner></Banner>
+      {/* <h2>This is Banner</h2> */}
     </div>
   );
 };
