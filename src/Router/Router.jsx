2c349b17f0c9b0efcc5ac0cd5f784b3c0cae0c16
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-11-08 23:06:39 +0600

    add a authentication

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,6 +2,10 @@ import { createBrowserRouter } from "react-router";
 import MainLayouts from "../Layouts/MainLayouts";
 import Home from "../Components/Home";
 import ErrorPage from "../Components/ErrorPage";
+import AllProducts from "../Components/AllProducts";
+import MyExports from "../Components/MyExports";
+import MyImports from "../Components/MyImports";
+import AddExport from "../Components/AddExport";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +16,22 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home></Home>,
+      },
+      {
+        path: "/allProducts",
+        element: <AllProducts></AllProducts>,
+      },
+      {
+        path: "/myExports",
+        element: <MyExports></MyExports>,
+      },
+      {
+        path: "/myImports",
+        element: <MyImports></MyImports>,
+      },
+      {
+        path: "/addExport",
+        element: <AddExport></AddExport>,
       },
     ],
   },
